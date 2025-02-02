3323ee3c023c3d1ea349532e547c28fe962a641e
Vasco-Luz <vascoalexluz2@gmail.com>
2025-02-02 01:06:37 +0000

    started working on the transcondutance cell structure in python

--- MY_pythn_lib/blocks_sky130.py
+++ MY_pythn_lib/blocks_sky130.py
@@ -94,8 +94,11 @@ class MOS:
 
 #class for Resistors
 class Resistor:
-    def __init__(self,res_val: float):
-        self.res_val = res_val 
+    def __init__(self,res_w: float,res_l: float,res_model: str, res_m: int):
+        self.res_w = res_w 
+        self.res_l = res_l
+        self.res_model = res_model
+        self.res_m = res_m
 
 
 #Basic structure, the same structure used in VA001 bias for 5V
@@ -105,7 +108,9 @@ class Resistor:
 class transcondutance_cell_v1_5V:
 
     #self initiation function
-    def __init__(self, W_M_1_2: float, L_M_1_2: float, m_M_1_2: int, W_M_3_4: float, L_M_3_4: float, m_M_3_4: int, W_M_5_6: float, L_M_5_6: float, m_M_5_6: int, R1: int):
+    def __init__(self, W_M_1_2: float, L_M_1_2: float, m_M_1_2: int, W_M_3_4: float, L_M_3_4: float, m_M_3_4: int, W_M_5_6: float,
+                  L_M_5_6: float, m_M_5_6: int,nf_M_1_2: int, nf_M_3_4: int, nf_M_5_6: int,P_model: str,N_upper_model:str,N_down_model:str,
+                  res_1_w : float,res_1_l: float,res_1_model: str,res_1_m: int ):
         self.W_M_1_2 = W_M_1_2
         self.L_M_1_2 = L_M_1_2
         self.m_M_1_2 = m_M_1_2
@@ -115,22 +120,37 @@ class transcondutance_cell_v1_5V:
         self.W_M_5_6 = W_M_5_6
         self.L_M_5_6 = L_M_5_6
         self.m_M_5_6 = m_M_5_6
-        self.R1 = R1
+        self.nf_M_1_2 = nf_M_1_2
+        self.nf_M_3_4 = nf_M_3_4
+        self.nf_M_5_6 = nf_M_5_6
+        self.P_model = P_model
+        self.N_upper_model = N_upper_model
+        self.N_down_model  = N_down_model
+        self.res_1_w = res_1_w
+        self.res_1_l = res_1_l 
+        self.res_1_model = res_1_model
+        self.res_1_m = res_1_m  
+    
+ 
+        self.mos1 = MOS(mos_type="Pmos", w_per_finger=W_M_1_2, gate_length=L_M_1_2, instance_name="M1", nf=nf_M_1_2, m=m_M_1_2, model=P_model)
+        self.mos2 = MOS(mos_type="Pmos", w_per_finger=W_M_1_2, gate_length=L_M_1_2, instance_name="M2", nf=nf_M_1_2, m=m_M_1_2, model=P_model)
+        self.mos3 = MOS(mos_type="Nmos", w_per_finger=W_M_3_4, gate_length=L_M_3_4, instance_name="M3", nf=nf_M_3_4, m=m_M_3_4, model=N_upper_model)
+        self.mos4 = MOS(mos_type="Nmos", w_per_finger=W_M_3_4, gate_length=L_M_3_4, instance_name="M4", nf=nf_M_3_4, m=m_M_3_4, model=N_upper_model)
+        self.mos5 = MOS(mos_type="Nmos", w_per_finger=W_M_5_6, gate_length=L_M_5_6, instance_name="M5", nf=nf_M_5_6, m=m_M_5_6, model=N_down_model)
+        self.mos6 = MOS(mos_type="Nmos", w_per_finger=W_M_5_6, gate_length=L_M_5_6, instance_name="M6", nf=nf_M_5_6, m=m_M_5_6, model=N_down_model)
+        self.res1= Resistor(res_w=res_1_w,res_l=res_1_l,res_model=res_1_model,res_m=res_1_m)
 
-        self.mos1 = MOS(mos_type="Pmos", w_per_finger=W_M_1_2, gate_length=L_M_1_2, instance_name="M1", nf=2, m=m_M_1_2, model="pfet_g5v0d10v5")
-        self.mos2 = MOS(mos_type="Pmos", w_per_finger=W_M_1_2, gate_length=L_M_1_2, instance_name="M2", nf=2, m=m_M_1_2, model="pfet_g5v0d10v5")
-        self.mos3 = MOS(mos_type="Nmos", w_per_finger=W_M_3_4, gate_length=L_M_3_4, instance_name="M3", nf=2, m=m_M_3_4, model="nfet_g5v0d10v5")
-        self.mos4 = MOS(mos_type="Nmos", w_per_finger=W_M_3_4, gate_length=L_M_3_4, instance_name="M4", nf=2, m=m_M_3_4, model="nfet_g5v0d10v5")
-        self.mos5 = MOS(mos_type="Nmos", w_per_finger=W_M_5_6, gate_length=L_M_5_6, instance_name="M5", nf=2, m=m_M_5_6, model="nfet_g5v0d10v5")
-        self.mos6 = MOS(mos_type="Nmos", w_per_finger=W_M_5_6, gate_length=L_M_5_6, instance_name="M6", nf=2, m=m_M_5_6, model="nfet_g5v0d10v5")
-        self.res1 = Resistor(res_val=R1)
+
+
 
     #create random individual for population
-    def create_random_individual():
-        W_M_1_2 = automation_support.random_value(1,100,1)
-        L_M_1_2 = automation_support.random_value(0.5,10,1)
-        m_M_1_2 = automation_support.random_value(1,100,0)
-        W_M_3_4 = automation_support.random_value(1,100,1)
+    def create_random_individual(W_min_pmos_val:float,L_min_pmos_val:float,W_min_nmos_midle_val:float):
+        W_M_1_2 = automation_support.random_value(W_min_pmos_val,20,0.1)
+        L_M_1_2 = automation_support.random_value(L_min_pmos_val,10,1)
+        m_M_1_2 = automation_support.random_value(2,80,2)
+
+
+        W_M_3_4 = automation_support.random_value(W_min_nmos_midle_val,20,0.1)
         L_M_3_4 = automation_support.random_value(0.5,10,1)
         m_M_3_4 = automation_support.random_value(1,100,0)
         W_M_5_6 = automation_support.random_value(1,100,1)
@@ -139,71 +159,3 @@ class transcondutance_cell_v1_5V:
         R1 = automation_support.random_value(1000,50000,0)
         return transcondutance_cell_v1_5V(W_M_1_2, L_M_1_2, m_M_1_2, W_M_3_4, L_M_3_4, m_M_3_4, W_M_5_6, L_M_5_6, m_M_5_6, R1)
     
-    #remove the simulation values
-    def remove_files(path_to_directory):
-        file_names = ["M1.txt", "M2.txt", "M3.txt", "M4.txt", "M5.txt", "M6.txt", "variation.txt"]
-        for file_name in file_names:
-            file_path = os.path.join(path_to_directory, file_name)
-            try:
-                os.remove(file_path)
-                print(f"Removed {file_path}")
-            except FileNotFoundError:
-                print(f"File {file_path} not found.")
-    
-
-    #substitute the variables of the spice file
-    def substitute_variables(spice_path,solution: 'TransconductanceCellV1_5V'):
-        with open(spice_path, 'r') as file:
-            lines = file.readlines()
-        with open(spice_path, 'w') as file:
-            for line in lines:
-                # Check for the .param line and update the value if the parameter matches
-                if re.match(r'\.param\s+W_M_1_2', line):
-                    line = f".param W_M_1_2={solution.W_M_1_2}\n"
-                if re.match(r'\.param\s+L_M_1_2', line):
-                    line = f".param L_M_1_2={solution.L_M_1_2}\n"
-                if re.match(r'\.param\s+m_M_1_2', line):
-                    line = f".param m_M_1_2={solution.m_M_1_2}\n"
-                if re.match(r'\.param\s+W_M_3_4', line):
-                    line = f".param W_M_3_4={solution.W_M_3_4}\n"
-                if re.match(r'\.param\s+L_M_3_4', line):
-                    line = f".param L_M_3_4={solution.L_M_3_4}\n"
-                if re.match(r'\.param\s+m_M_3_4', line):
-                    line = f".param m_M_3_4={solution.m_M_3_4}\n"
-                if re.match(r'\.param\s+W_M_5_6', line):
-                    line = f".param W_M_5_6={solution.W_M_5_6}\n"
-                if re.match(r'\.param\s+L_M_5_6', line):
-                    line = f".param L_M_5_6={solution.L_M_5_6}\n"
-                if re.match(r'\.param\s+m_M_5_6', line):
-                    line = f".param m_M_5_6={solution.m_M_5_6}\n"
-                if re.match(r'\.param\s+R1', line):
-                    line = f".param R1={solution.R1}\n"
-                file.write(line)
-
-
-
-
-    #fitness function
-    def fitness(solution: 'TransconductanceCellV1_5V', solution_idx :int, path_to_directory,path_to_spice):
-        transcondutance_cell_v1_5V.remove_files(path_to_directory)
-        transcondutance_cell_v1_5V.substitute_variables(path_to_spice,solution)
-        sim_comands.ngspice_sim(path_to_spice)
-
-        solution.mos1 = MOS.transistor_OV_and_current(solution.mos1,path_to_directory+"/M1.txt")
-
-        solution.mos2 = MOS.transistor_OV_and_current(solution.mos2,path_to_directory+"/M2.txt")
-
-        solution.mos3 = MOS.transistor_OV_and_current(solution.mos3,path_to_directory+"/M3.txt")
-
-        solution.mos4 = MOS.transistor_OV_and_current(solution.mos4,path_to_directory+"/M4.txt")
-
-        solution.mos5 = MOS.transistor_OV_and_current(solution.mos5,path_to_directory+"/M5.txt")
-
-        solution.mos6 = MOS.transistor_OV_and_current(solution.mos6,path_to_directory+"/M6.txt")
-
-        Curr_var= abs(solution.mos1.ids-solution.mos2.ids)
-
-        fitness = 100*Curr_var/((solution.mos1.ids+solution.mos2.ids)/2)
-
-        return fitness
-
